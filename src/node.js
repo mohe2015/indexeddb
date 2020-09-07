5515b941e17488afd9b293829a5c814a872fbaaa
mohe2015 <Moritz.Hedtke@t-online.de>
2020-09-07 18:34:44 +0200

    More code

--- src/node.js
+++ src/node.js
@@ -16,6 +16,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 // @ts-check
+
 import { MongoClient } from 'mongodb';
 
 // https://docs.mongodb.com/drivers/node/
--- test/index.js
+++ test/index.js
@@ -1,3 +1,22 @@
-import { connect } from "indexeddb";
+/*
+@dev.mohe/indexeddb - Make a database interface available that works in the browser and in nodejs
+Copyright (C) 2020 Moritz Hedtke <Moritz.Hedtke@t-online.de>
 
-let database = connect("blub", 1);
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+// @ts-check
+
+import { create } from "../src/browser";
+
+let database = await create("blub", 1);
