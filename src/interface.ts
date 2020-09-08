a5fa955d0242b58a9ee0698c51c6fb78a080c423
mohe2015 <Moritz.Hedtke@t-online.de>
2020-09-08 16:39:37 +0200

    epic type hacks

--- src/interface.ts
+++ src/interface.ts
@@ -101,7 +101,7 @@ function test<A extends { [a: string]: DatabaseSchemaIndex; }, B extends (keyof 
     return fromEntries(filteredEntries)
 }
 
-function migrate<T extends IsNever<keyof A & B>, A extends { [a: string]: DatabaseSchemaIndex; } = { [a: string]: DatabaseSchemaIndex; }, B extends keyof A = keyof A, C extends { [a: string]: DatabaseSchemaIndex; } = { [a: string]: DatabaseSchemaIndex; }>(alwaysTrue: T, state: A, migration: Migration<A, B, C>): Id<Pick<A & C, Exclude<keyof A, Extract<keyof A, B>> | Exclude<keyof C, Extract<keyof A, B>>>> {
+function migrate<T extends IsNever<keyof A & keyof C>, A extends { [a: string]: DatabaseSchemaIndex; } = { [a: string]: DatabaseSchemaIndex; }, B extends keyof A = keyof A, C extends { [a: string]: DatabaseSchemaIndex; } = { [a: string]: DatabaseSchemaIndex; }>(alwaysTrue: T, state: A, migration: Migration<A, B, C>): Id<Pick<A & C, Exclude<keyof A, Extract<keyof A, B>> | Exclude<keyof C, Extract<keyof A, B>>>> {
     let merged = merge(state, migration.addedIndexes)
     let removed = test(merged, migration.removedIndexes)
     return removed 
@@ -124,14 +124,12 @@ let migration1 = {
 let a = {}
 let merged = migrate(true, a, migration1)
 
-/*
 let migration2: Migration<typeof merged, "test.name", {}> = {
     addedIndexes: {},
     removedIndexes: ["test.name"] as const
-}*/
+}
 
-//typeAssert<IsNever<keyof typeof merged & keyof typeof migration1.addedIndexes>>(true);
-//let merged1 = migrate(merged, migration1)
+let merged1 = migrate(true, merged, migration2)
 
 // TODO FIXME state and migration need to be connected
 // currently they only fail when the migration is incompatible
