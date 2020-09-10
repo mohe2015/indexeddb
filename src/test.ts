267d0385df47d34ea0bb70f5ef64de0ed2c59154
mohe2015 <Moritz.Hedtke@t-online.de>
2020-09-10 21:52:20 +0200

    seems to work

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,4 @@
-export 
+type Id<T extends object> = {} & { [P in keyof T]: T[P] }
 
 let state = {
     users: {
@@ -19,15 +19,15 @@ let state = {
     }
 } as const
 
-let remove = {"users": {"username":null}} as const
+let remove = {"users": {"username":null, "password": null}, "posts": {"title": null}} as const
 
 function test<STATE extends { [a: string]: { [a: string]: any } }, REMOVE extends { [K in keyof STATE]?: { [K1 in keyof STATE[K]]?: any } }>
                 (_objectStores: STATE, removeObjectStores: REMOVE): 
-                {
+                Id<{
                     [K in keyof STATE]: Pick<STATE[K], Exclude<keyof STATE[K], keyof REMOVE[K]>>
-                }
+                }>
                 {
                     return null as any
                 }
 
-let value = test(state, remove)
+export let value = test(state, remove)
