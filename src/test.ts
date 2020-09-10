2f3084fc19a5c50daf4f74c15f7fdbdfa042562a
mohe2015 <Moritz.Hedtke@t-online.de>
2020-09-10 21:48:56 +0200

    This may work

--- src/test.ts
+++ src/test.ts
@@ -19,13 +19,12 @@ let state = {
     }
 } as const
 
-let remove = {"users": ["username"]} as const
+let remove = {"users": {"username":null}} as const
 
-
-function test<STATE extends { [a: string]: { [a: string]: any } }, REMOVE extends { [K in keyof STATE]?: readonly (keyof STATE[K])[] }>
+function test<STATE extends { [a: string]: { [a: string]: any } }, REMOVE extends { [K in keyof STATE]?: { [K1 in keyof STATE[K]]?: any } }>
                 (_objectStores: STATE, removeObjectStores: REMOVE): 
                 {
-                    [K in keyof STATE]: Pick<STATE[K], Exclude<keyof STATE[K], REMOVE[K]>>
+                    [K in keyof STATE]: Pick<STATE[K], Exclude<keyof STATE[K], keyof REMOVE[K]>>
                 }
                 {
                     return null as any
