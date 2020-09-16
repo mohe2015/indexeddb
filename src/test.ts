f0e51a4a7fe8c19808b56eab196dc3005c2aaac6
mohe2015 <Moritz.Hedtke@t-online.de>
2020-09-16 17:10:08 +0200

    Add generic objectstores parameter

--- src/test.ts
+++ src/test.ts
@@ -1,40 +1,51 @@
-export type TestMigration<FROMVERSION extends number, TOVERSION extends number, ADDED extends TestObjectStores, REMOVED extends TestObjectStores> = {
+export type TestMigration<FROMVERSION extends number, TOVERSION extends number, ADDED extends TestObjectStores, REMOVED extends TestObjectStores, OBJECTSTORES extends TestObjectStores> = {
     fromVersion: FROMVERSION
     toVersion: TOVERSION
-    baseSchema: TestSchemaWithoutMigration<FROMVERSION>
+    baseSchema: TestSchemaWithoutMigration<FROMVERSION, OBJECTSTORES>
     addedColumns: ADDED
     removedColumns: REMOVED
 }
 
 export type TestObjectStores = { [a: string]: any; };
 
-export type TestSchemaWithoutMigration<VERSION extends number> = {
+export type TestSchemaWithoutMigration<VERSION extends number, OBJECTSTORES extends TestObjectStores> = {
     version: VERSION,
-    objectStores: TestObjectStores
+    objectStores: OBJECTSTORES
 }
 
-export type TestSchemaWithMigration<VERSION extends number, FROMVERSION extends number, TOVERSION extends number, ADDED extends TestObjectStores, REMOVED extends TestObjectStores> = TestSchemaWithoutMigration<VERSION> & {
-    migration: TestMigration<FROMVERSION, TOVERSION, ADDED, REMOVED> | null
+export type TestSchemaWithMigration<VERSION extends number, FROMVERSION extends number, TOVERSION extends number, ADDED extends TestObjectStores, REMOVED extends TestObjectStores, OBJECTSTORES extends TestObjectStores> = TestSchemaWithoutMigration<VERSION, OBJECTSTORES & ADDED> & {
+    migration: TestMigration<FROMVERSION, TOVERSION, ADDED, REMOVED, OBJECTSTORES> | null
 }
 
-let initialSchema: TestSchemaWithoutMigration<1> = {
+let initialSchema: TestSchemaWithoutMigration<1, {}> = {
     version: 1,
     objectStores: {}
 }
 
-let migration: TestMigration<1, 2, {}, {}> = {
+let addedColumns = {
+    users: {
+        name: {
+
+        },
+        password: {
+
+        }
+    }
+}
+
+let migration: TestMigration<1, 2, typeof addedColumns, {}, {}> = {
     fromVersion: initialSchema.version,
     toVersion: 2,
     baseSchema: initialSchema,
-    addedColumns: {},
+    addedColumns,
     removedColumns: {},
 }
 
-function migrate<FROMVERSION extends number, TOVERSION extends number, ADDED extends TestObjectStores, REMOVED extends TestObjectStores>(migration: TestMigration<FROMVERSION, TOVERSION, ADDED, REMOVED>): TestSchemaWithMigration<TOVERSION, FROMVERSION, TOVERSION, ADDED, REMOVED> {
+function migrate<FROMVERSION extends number, TOVERSION extends number, ADDED extends TestObjectStores, REMOVED extends TestObjectStores, OBJECTSTORES extends TestObjectStores>(migration: TestMigration<FROMVERSION, TOVERSION, ADDED, REMOVED, OBJECTSTORES>): TestSchemaWithMigration<TOVERSION, FROMVERSION, TOVERSION, ADDED, REMOVED, OBJECTSTORES> {
     return {
         migration,
         version: migration.toVersion,
-        objectStores: migration.addedColumns
+        objectStores: Object.assign({}, migration.baseSchema.objectStores, migration.addedColumns) as OBJECTSTORES & ADDED // TODO FIXME this is actually a wrong implementation
     }
 }
 
