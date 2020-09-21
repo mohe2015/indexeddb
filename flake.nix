8565c0a3aeec38e1179b3e0913f09c96c720c21c
mohe2015 <Moritz.Hedtke@t-online.de>
2020-09-21 19:29:44 +0200

    Add flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "indexeddb";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShell.${system} = pkgs.mkShell {
+        buildInputs = [
+          pkgs.yarn
+          pkgs.nodejs
+          pkgs.nodePackages.npm-check-updates
+        ];
+      };
+    };
+}
