32f1ab0396cd1ca65f4520bc486fb16fd2d21c61
mohe2015 <Moritz.Hedtke@t-online.de>
2020-09-22 17:00:07 +0200

    Why the heck?!?!

--- flake.nix
+++ flake.nix
@@ -1,15 +1,17 @@
 {
+  # https://github.com/NixOS/nix/issues/3803
   description = "indexeddb";
 
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      yarn = pkgs.yarn.override { nodejs = pkgs.nodejs-14_x; };
     in
     {
       devShell.${system} = pkgs.mkShell {
         buildInputs = [
-          pkgs.yarn
+          yarn
           pkgs.nodejs-14_x
           pkgs.nodePackages.npm-check-updates
         ];
